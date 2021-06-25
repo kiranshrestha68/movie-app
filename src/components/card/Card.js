823ea2965f27399725cd1a43c8a49a0f01788d51
kiranshrestha68 <kiranstha68@gmail.com>
2021-06-25 07:37:20 +0545

    card updated

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -9,22 +9,20 @@ const Card = () => {
         alt=""
         className="photos"
       />
-      <div className="title">
-        Loki
-      </div>
+      <div className="title">Loki</div>
       <div className="category">
         <div className="time-date">
           <span className="date"> 2021</span>
           <span> 50min </span>
         </div>
 
-        <div className="cat__type">
-           Series
-        </div>
+        <div className="cat__type">Series</div>
       </div>
       <div className="desc">
-             Sit id ut velit exercitation occaecat esse eiusmod aute commodo irure id
-        elit sunt.
+        After stealing the Tesseract during the events of “Avengers: Endgame,”
+        an alternate version of Loki is brought to the mysterious Time Variance
+        Authority, a bureaucratic organization that exists outside of time and
+        space and monitors the timeline.
       </div>
     </div>
   );
