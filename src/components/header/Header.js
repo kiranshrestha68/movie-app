9a55168b0b2cda21212267747f71d00290a420de
kiranshrestha68 <kiranstha68@gmail.com>
2021-06-25 17:07:47 +0545

    search icon added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./header.css";
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from "@material-ui/icons/Search";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="left__header">MovieApp</div>
       <div className="right__header">
-       
-        <input className="search__input" />
-         <div className="search"> <SearchIcon/></div>
+        <div className="search__input">
+          <input />
+        </div>
+
+        <div className="search">
+          {" "}
+          <SearchIcon />
+        </div>
       </div>
     </div>
   );
